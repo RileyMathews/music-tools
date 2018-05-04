d7ecc2a0e970aba56bfb0b777e3ea8f0a720b96b
Riley Mathews <rileymathews80@gmail.com>
2018-05-04 12:58:24 -0500

    added nav functionality for remaining pages

---
+++ src/scripts/chords/index.js
@@ -0,0 +1,11 @@
+//index js file for intervals directory
+//function to call the intervals tool page
+const buildIntervalTools = () => {
+    const fragment = document.createDocumentFragment()
+    const placeholderHeader = document.createElement("h1")
+    placeholderHeader.textContent = "Chrds"
+    fragment.appendChild(placeholderHeader)
+    return fragment
+}
+
+module.exports = buildIntervalTools
--- src/scripts/navEventHandlers.js
+++ src/scripts/navEventHandlers.js
@@ -1,21 +1,38 @@
 //function to activate nav buttons
 const welcomePageBuilder = require("./welcomePageBuilder")
 const intervalPageBuilder = require("./intervals")
+const metronomePageBuilder = require("./metronome")
+const chordPageBuilder = require("./chords")
+const modePageBuilder = require("./modes")
 const nukeHTML = require("./nukeHTML")
 const appendHTML = require("./appendHTML")
+
 
 const navButtons = () => {
     $("#nav__home").on("click", () => {
         nukeHTML("#main--output")
         appendHTML(welcomePageBuilder(), "#main--output")
     })
+
     $("#nav__intervals").on("click", () => {
         nukeHTML("#main--output")
         appendHTML(intervalPageBuilder(), "#main--output")
     })
-    $("#nav__metronome").on("click", () => {})
-    $("#nav__chords").on("click", () => {})
-    $("#nav__modes").on("click", () => {})
+
+    $("#nav__metronome").on("click", () => {
+        nukeHTML("#main--output")
+        appendHTML(metronomePageBuilder(), "#main--output")
+    })
+
+    $("#nav__chords").on("click", () => {
+    nukeHTML("#main--output")
+    appendHTML(chordPageBuilder(), "#main--output")
+    })
+
+    $("#nav__modes").on("click", () => {
+    nukeHTML("#main--output")
+    appendHTML(modePageBuilder(), "#main--output")
+    })
 }
 
 module.exports = navButtons
